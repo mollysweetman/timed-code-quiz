2220416f0b1ca6825c086c80a84ff0fd922a2196
mollysweetman <67077842+mollysweetman@users.noreply.github.com>
2020-08-15 08:11:52 -0600

    adding functions to quiz.js, trying to get something running

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,49 +1,87 @@
-var quizContainer = document.getElementById('quiz');
+var quizContainer = document.getElementById('container');
 var resultsContainer = document.querySelector('.score-container');
 var initials = document.querySelector('.initials-container');
-var submitButton = document.getElementById('submit');
+const startButton = document.getElementById('start');
+var  = document.getElementById('questions');
+
+var answerButtons = document.getElementById('answer-btn');
 
 
-hideElement(resultsContainer);
-hideElement(initials);
 
+// hideElement(resultsContainer);
+// hideElement(initials);
+
+var startTimer = document.querySelector('.start')
+var showTime = document.querySelector('#test-time');
 var testTime = 60;
+var secondsInterval;
 
-setInterval(function(){
+
+startTimer.addEventListener('click', function(){
+  secondsInterval = setInterval(function() {
     testTime--;
-
-    if (testTime >= 0){
-        id = document.getElementById("testTime");
-        id.innerHTML = testTime;
-
+    showTime.innerText = testTime;
+    if (testTime === 0) {
+      clearInterval(secondsInterval);
     }
-    if (testTime === 0){
-        id.innerHTML = "Test Complete";
-   
-    
-    };
 
-}, 1000);
+  }, 1000   );
 
-function startQuiz(questions) {
-  // first question displays
-  showQuestions(questions[0]);
-  // timer starts
-  // show score
-  console.log(questions);
-}
+})
 
 
 
+// function showElement(elementToShow) {
+// elementToShow.style.display = 'block'
 
-function showElement(elementToShow) {
-elementToShow.style.display = 'block'
-
-}
-function hideElement(elementToHide) {
-  elementToHide.style.display = 'none'
+// }
+// function hideElement(elementToHide) {
+//   elementToHide.style.display = 'none'
   
-  }
+//   }
+
+
+
+
+// function startQuiz(questions) {
+//   // first question displays
+//   showQuestions(questions[0]);
+//   // timer starts
+//   // show score
+  
+// }
+
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+  
+// go to next question
+gotoNextQuestion()
+
+}
+
+
+
+function gotoNextQuestion() {
+showNextQuestions
+
+}
+
+
+
+function showNextQuestion(questions){
+qu
+
+}
+
+
+function chooseAnswer() {
+
+}
+
+
+
 
 var questions = [
     {
@@ -80,45 +118,44 @@ var questions = [
     }
   ];
 
-  startQuiz(questions);
 
-function showQuestions(question) {
+
+// function showQuestions(question) {
   // adding variables for the output and answer choices
   // var userOptions = [];
   // var answers;
 
 
-  // for (var i=0; i<questions[i]; i++) {
+//   // for (var i=0; i<questions[i]; i++) {
 
-  //   //shows the list of answers for question
-  //   answers = [];
-  // }
-  console.log(question);
-var quizQuestions = document.querySelector('.questions');
-quizQuestions.innerText = question.testQuestions
+//   //   //shows the list of answers for question
+//   //   answers = [];
 
-
-
- }
+// var quizQuestions = document.querySelector('.questions');
+// quizQuestions.innerText = question.testQuestions
 
 
 
-// button to start the quiz
-// timer starts
-// select an answer to first question
-// select an answer to second question
-// Enter in initials to store user score
-// displays high score 
-// function for start and next buttons
-// function for checkbox or radio button of the questions
-// function for timer counter
-// count the score
-// display the score and initials when either time counts to 0 or all questions get answered
+//  }
 
+
+
+// // button to start the quiz
+// // timer starts
+// // select an answer to first question
+// // select an answer to second question
+// // Enter in initials to store user score
+// // displays high score 
+// // function for start and next buttons
+// // function for checkbox or radio button of the questions
+// // function for timer counter
+// // count the score
+// // display the score and initials when either time counts to 0 or all questions get answered
 
 
 
 
-// submitButton.onclick = function() {
-//   showResults(questions)
-// }
+
+// // submitButton.onclick = function() {
+// //   showResults(questions)
+// // }
