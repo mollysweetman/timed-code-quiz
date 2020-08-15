429a3cb0e3d75ead3decb4fc2391b4afce1a3efc
mollysweetman <67077842+mollysweetman@users.noreply.github.com>
2020-08-14 18:04:45 -0600

    adding code to quiz.js

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,6 +1,11 @@
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.querySelector('.score-container');
+var initials = document.querySelector('.initials-container');
+var submitButton = document.getElementById('submit');
 
 
-
+hideElement(resultsContainer);
+hideElement(initials);
 
 var testTime = 60;
 
@@ -20,28 +25,39 @@ setInterval(function(){
 
 }, 1000);
 
-var quiz = document.getElementById('quiz');
-var results = document.getElementById('results');
-var submitButton = document.getElementById('submit');
+function startQuiz(questions) {
+  // first question displays
+  showQuestions(questions[0]);
+  // timer starts
+  // show score
+  console.log(questions);
+}
 
 
 
 
+function showElement(elementToShow) {
+elementToShow.style.display = 'block'
 
-    
+}
+function hideElement(elementToHide) {
+  elementToHide.style.display = 'none'
+  
+  }
+
 var questions = [
     {
-      title: "Commonly used data types DO NOT include:",
+      testQuestions: "Commonly used data types DO NOT include:",
       choices: ["strings", "booleans", "alerts", "numbers"],
       answer: "alerts"
     }, 
     {
-      title: "The condition in an if / else statement is enclosed within ____.",
+      testQuestions: "The condition in an if / else statement is enclosed within ____.",
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
       answer: "parentheses"
     },
     {
-      title: "Arrays in JavaScript can be used to store ____.",
+      testQuestions: "Arrays in JavaScript can be used to store ____.",
       choices: [
         "numbers and strings",
         "other arrays",
@@ -51,37 +67,58 @@ var questions = [
       answer: "all of the above"
     },
     {
-      title:
+      testQuestions:
         "String values must be enclosed within ____ when being assigned to variables.",
       choices: ["commas", "curly brackets", "quotes", "parentheses"],
       answer: "quotes"
     },
     {
-      title:
+      testQuestions:
         "A very useful tool used during development and debugging for printing content to the debugger is:",
       choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
       answer: "console.log"
     }
   ];
 
-function showQuestions(questions) {
+  startQuiz(questions);
+
+function showQuestions(question) {
   // adding variables for the output and answer choices
-  var options = [];
-  var answers;
+  // var userOptions = [];
+  // var answers;
 
 
-  for (var i=0; i<questions[i]; i++) {
+  // for (var i=0; i<questions[i]; i++) {
 
-    //shows the list of answers for question
-    answers = [];
-  }
-
-}
-
-
-
+  //   //shows the list of answers for question
+  //   answers = [];
+  // }
+  console.log(question);
+var quizQuestions = document.querySelector('.questions');
+quizQuestions.innerText = question.testQuestions
 
 
-submitButton.onclick = function() {
-  showResults(questions)
-}
+
+ }
+
+
+
+// button to start the quiz
+// timer starts
+// select an answer to first question
+// select an answer to second question
+// Enter in initials to store user score
+// displays high score 
+// function for start and next buttons
+// function for checkbox or radio button of the questions
+// function for timer counter
+// count the score
+// display the score and initials when either time counts to 0 or all questions get answered
+
+
+
+
+
+// submitButton.onclick = function() {
+//   showResults(questions)
+// }
