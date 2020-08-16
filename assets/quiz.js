8614bcaf09077c092b6ff331ccd08716008ae57b
mollysweetman <67077842+mollysweetman@users.noreply.github.com>
2020-08-15 18:09:30 -0600

    fixing errors in quiz.js

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,15 +1,15 @@
 var quizContainer = document.getElementById('container');
 var resultsContainer = document.querySelector('.score-container');
 var initials = document.querySelector('.initials-container');
-const startButton = document.getElementById('start');
-var  = document.getElementById('questions');
+var startButton = document.getElementById('start');
 
+var questionItem = document.getElementById('questions');
 var answerButtons = document.getElementById('answer-btn');
+var addScore = 0;
 
 
-
-// hideElement(resultsContainer);
-// hideElement(initials);
+hideElement(resultsContainer);
+hideElement(initials);
 
 var startTimer = document.querySelector('.start')
 var showTime = document.querySelector('#test-time');
@@ -17,29 +17,16 @@ var testTime = 60;
 var secondsInterval;
 
 
-startTimer.addEventListener('click', function(){
-  secondsInterval = setInterval(function() {
-    testTime--;
-    showTime.innerText = testTime;
-    if (testTime === 0) {
-      clearInterval(secondsInterval);
-    }
 
-  }, 1000   );
-
-})
-
-
-
-// function showElement(elementToShow) {
-// elementToShow.style.display = 'block'
-
-// }
-// function hideElement(elementToHide) {
-//   elementToHide.style.display = 'none'
+function showElement(elementToShow) {
+  elementToShow.style.display = 'block'
   
-//   }
-
+  }
+  function hideElement(elementToHide) {
+    elementToHide.style.display = 'none'
+  
+    }
+    
 
 
 
@@ -48,75 +35,132 @@ startTimer.addEventListener('click', function(){
 //   showQuestions(questions[0]);
 //   // timer starts
 //   // show score
-  
+
+// }
+
+startTimer.addEventListener('click', function () {
+  startTimer.style.display = 'none';
+  gotoNextQuestion();
+  secondsInterval = setInterval(function () {
+    testTime--;
+    showTime.innerText = testTime;
+    if (testTime === 0) {
+      clearInterval(secondsInterval);
+    }
+
+  }, 1000);
+
+})
+
+
+
+function startGame() {
+ startButton.addEventListener('click', startGame())
+  startButton.style.display = 'hide';
+
+  var choiceButton = document.createElement("button");
+  choiceButton.setAttribute("class" ,"options");
+  choiceButton.setAttribute("value" , options);
+  choiceButton.textContent = options;
+  choiceButton.onclick = answer;
+
+
+var q1 = document.createElement("button")
+var q2 = document.createElement("button")
+var q3 = document.createElement("button")
+var q4 = document.createElement("button")
+
+//   <button onclick="myFunction()">Click me</button>
+// object.addEventListener("click", myScript);
+
+
+  // go to next question
+  //gotoNextQuestion()
+//   addScore++;
+// answer = questions[addScore].answer
+
+// questionItem.textContent = questions[addScore].title
+// options.innerHTML = '';
+
+
+
+
+// if (event.target.value === answerButtons);
+// console.log(questions);
+}
+
+
+
+
+
+
+// function showNextQuestion(questions) {
+//   // questionItem.innerText = 
+//   startButton.setAttribute('start');
+
 // }
 
 
-startButton.addEventListener('click', startGame)
+// function chooseAnswer() {
 
-function startGame() {
-  
-// go to next question
-gotoNextQuestion()
-
-}
-
-
-
-function gotoNextQuestion() {
-showNextQuestions
-
-}
-
-
-
-function showNextQuestion(questions){
-qu
-
-}
-
-
-function chooseAnswer() {
-
-}
-
-
+// }
 
 
 var questions = [
-    {
-      testQuestions: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    }, 
-    {
-      testQuestions: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    {
-      testQuestions: "Arrays in JavaScript can be used to store ____.",
-      choices: [
-        "numbers and strings",
-        "other arrays",
-        "booleans",
-        "all of the above"
-      ],
-      answer: "all of the above"
-    },
-    {
-      testQuestions:
-        "String values must be enclosed within ____ when being assigned to variables.",
-      choices: ["commas", "curly brackets", "quotes", "parentheses"],
-      answer: "quotes"
-    },
-    {
-      testQuestions:
-        "A very useful tool used during development and debugging for printing content to the debugger is:",
-      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-      answer: "console.log"
-    }
-  ];
+  {
+    testQuestions: "Commonly used data types DO NOT include:",
+    options: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    testQuestions: "The condition in an if / else statement is enclosed within ____.",
+    options: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    testQuestions: "Arrays in JavaScript can be used to store ____.",
+    options: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ],
+    answer: "all of the above"
+  },
+  {
+    testQuestions:
+      "String values must be enclosed within ____ when being assigned to variables.",
+    options: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes"
+  },
+  {
+    testQuestions:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    options: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    answer: "console.log"
+  }
+];
+
+
+function gotoNextQuestion() {
+  //showNextQuestions
+
+  var questionIndex = 0;
+  var questionsPath = document.querySelector("#questions");
+
+  questionsPath.innerText = questions[questionIndex].testQuestions;
+
+  // var posAns1 = questions[questionIndex].options
+  // var posAns2 = questions[questionIndex].options
+  // var posAns3 = questions[questionIndex].options
+  // var posAns4 = questions[questionIndex].options
+
+
+
+
+
+
+}
 
 
 
@@ -159,3 +203,4 @@ var questions = [
 // // submitButton.onclick = function() {
 // //   showResults(questions)
 // // }
+
